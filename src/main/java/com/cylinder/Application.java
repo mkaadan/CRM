b11da8c561b350d97d69b559105f18e56468de21
rpiper <rpiper@DESKTOP-RUBM00G.gateway.sasktel.net>
2017-10-01 21:35:21 -0600

    Add a general config for spring boot. Closes #10.

---
+++ src/main/java/com/cylinder/Application.java
@@ -0,0 +1,16 @@
+package com.cylinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.cylinder"})
+@SpringBootApplication
+public class Application {
+
+// Entry point to the application. Scans all of com.cylinder for springframework
+// annotations and maps the logic. 
+public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+}
+}
