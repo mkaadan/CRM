761de607584ddf95e252575f8a3fdd20248e8fce
Ryan Piper <ryp123@drone7.usask.ca>
2017-10-13 14:05:14 -0600

    Add more basic mapping to leads; add single record function and view

---
+++ src/main/java/com/cylinder/leads/LeadAddressRepository.java
@@ -0,0 +1,8 @@
+package com.cylinder.leads;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LeadAddressRepository extends CrudRepository<LeadAddress, Long> {
+}
