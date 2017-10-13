08a613e666d65d618bf7d4ce32108e6c01dde3b5
rpiper <rpiper@DESKTOP-RUBM00G.gateway.sasktel.net>
2017-10-13 08:09:34 -0600

    Autowired lead repository

--- src/main/java/com/cylinder/leads/LeadsController.java
+++ src/main/java/com/cylinder/leads/LeadsController.java
@@ -3,6 +3,8 @@ package com.cylinder.leads;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 import com.cylinder.leads.Lead;
 import com.cylinder.leads.LeadRepository;
@@ -11,12 +13,13 @@ import com.cylinder.leads.LeadRepository;
 @Controller
 @RequestMapping("/")
 public class LeadsController {
-
+      @Autowired
       private LeadRepository leadRepository;
 
     	@RequestMapping(method=RequestMethod.GET)
       // Render the list view for leads.
-    	public @ResponseBody Iterable<Lead> index(){
-        return leadRepository.findAll();
+    	public @ResponseBody String index(){
+        Lead lead = leadRepository.findOne(1L);
+         return lead.toString();
       }
 }
