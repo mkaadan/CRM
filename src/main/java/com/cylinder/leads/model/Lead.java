0302cbdc483830ffc4c108993e9ac0da71f1dfdc
rpiper <rpiper@rpipercomp.gateway.sasktel.net>
2017-11-03 11:30:57 -0600

    Add validation methods and missed setter methods

--- src/main/java/com/cylinder/leads/model/Lead.java
+++ src/main/java/com/cylinder/leads/model/Lead.java
@@ -1,24 +1,32 @@
 package com.cylinder.leads.model;
 
 import com.cylinder.global.Industry;
+
 import java.util.List;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="details", schema="lead")
 public class Lead {
     @Id
     @Column(name="lead_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long leadId;
     /** The first name of the lead. */
     @Column(name="first_name")
+    @Pattern(regexp="([a-zA-Z])", message="Please enter a valid first name.")
+
     protected String firstName;
 
     @Column(name="last_name")
+    @NotNull(message="Please provide the last name for the lead.")
+    @Pattern(regexp="[a-zA-Z]", message="Please provide a valid last name.")
     protected String lastName;
 
     @Column(name="title")
+    @Pattern(regexp="[a-zA-Z]", message="Please provide a valid title.")
     protected String title;
 
     @Column(name="company_name")
@@ -48,7 +56,7 @@ public class Lead {
       schema="lead",
       joinColumns={ @JoinColumn(name="lead_id", referencedColumnName="lead_id") },
       inverseJoinColumns={ @JoinColumn(name="email_id", referencedColumnName="email_id") }
-   )
+    )
     protected List<Email> emails;
 
     @OneToOne(cascade = CascadeType.ALL)
@@ -56,11 +64,10 @@ public class Lead {
     protected Source source;
 
     @Column
+    @Pattern(regexp="@.*", message="Please provide a valid twitter handler.")
     protected String twitter;
 
-
-    /** Construct for JPA **/
-    protected Lead() {}
+    public Lead() {}
 
   public Long getLeadId() {
       return this.leadId;
@@ -110,12 +117,48 @@ public class Lead {
     return this.status;
   }
 
+  public Source getSource() {
+    return this.source;
+  }
+
+  public String getTwitter() {
+    return this.twitter;
+  }
+
   public void setLeadId(long id) {
       this.leadId = id;
   }
 
   public void setFirstName(String name) {
     this.firstName = name;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setCompanyName(String compName) {
+    this.companyName = compName;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public void setstatus(Status status) {
+    this.status = status;
+  }
+
+  public void setSource(Source source) {
+    this.source = source;
+  }
+
+  public void setmobile(String mobile) {
+    this.mobile = mobile;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
   }
 
   public void setIndustry(Industry industry) {
@@ -128,5 +171,9 @@ public class Lead {
 
   public void setAddress(Address address) {
     this.address = address;
+  }
+
+  public void setTwitter(String twitter) {
+    this.twitter = twitter;
   }
 }
