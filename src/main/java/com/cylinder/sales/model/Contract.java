cf5f9d42a41768df4d4340ecfff00ba4008320aa
Unknown <austin.clarke.peters@gmail.com>
2017-11-14 12:29:08 -0600

    Added models for contracts, purchase orders and adresses

---
+++ src/main/java/com/cylinder/sales/model/Contract.java
@@ -0,0 +1,49 @@
+package com.cylinder.sales.contracts.model;
+
+import com.cylinder.crmusers.model.CrmUser;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "contracts", schema = "sale")
+public class Contract {
+    @Id
+    @Column(name="contract_id")
+    private Long contractId;
+
+    /** The contrac's title. */
+    @Column(name="contract_title")
+    private string contractTitle;
+
+    /** The contrac's body text. */
+    @Column(name="contract")
+    private string contractText;
+
+    public Contract() { }
+
+    /* Get the identifier for the contract. */
+    public long getContractId() {
+        return contractId;
+    }
+
+    /* Get the title of the contract. */
+    public String getContractTitle() { return contractTitle; }
+
+    /* Get the body text of the contract. */
+    public String getContractText() { return contractText; }
+
+    /* Set the identifier for the lead. */
+    public void setContractId(long contractId) {
+        this.contractId = contractId;
+    }
+
+    /* Set the title of the contract. */
+    public void setContractTitle(String contractTitle) {
+        this.contractTitle = contractTitle;
+    }
+
+    /* Set the body text of the contract. */
+    public void setContractText(String contractText) {
+        this.contractText = contractText;
+    }
+}
---
+++ src/main/java/com/cylinder/sales/model/ContractRepository.java
@@ -0,0 +1,8 @@
+package com.cylinder.sales.model;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContractRepository extends CrudRepository<Status, Long> {
+}
