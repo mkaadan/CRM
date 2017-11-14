3ad2052c4d87e2b3aae7001794f8f9d5973f558f
Unknown <austin.clarke.peters@gmail.com>
2017-11-14 15:24:10 -0600

    Added quotes list view

---
+++ src/main/resources/static/js/sales/quotesList.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    var table = $('#quoteList').DataTable( {
+        scrollY:        "300px",
+        scrollX:        true,
+        scrollCollapse: true,
+        paging:         false,
+        fixedColumns:   {
+            heightMatch: 'none'
+        }
+    } );
+} );
+
+$(document).ready(function() {
+    $(".glyphicon-trash").click(function(event) {
+        var thisRecord = $(this);
+        if (confirm("Are you sure you wish to delete this record?")) {
+          var id = thisRecord.data("id");
+          $.ajax({
+                  url: '/quote/records/' + id,
+                  type: 'DELETE',
+                  success: function(result) {
+                      var table = $('#quoteList').DataTable();
+                      var row = thisRecord.parents('tr');
+                      table.row(row)
+                           .remove()
+                           .draw();
+                  }
+              });
+        }
+    });
+});
