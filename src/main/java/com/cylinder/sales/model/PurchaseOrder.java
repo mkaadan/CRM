7f542b22bebe902494467bd453c00d454df6abfa
Unknown <austin.clarke.peters@gmail.com>
2017-11-14 13:25:01 -0600

    Added models for sales order and quote

--- src/main/java/com/cylinder/sales/model/PurchaseOrder.java
+++ src/main/java/com/cylinder/sales/model/PurchaseOrder.java
@@ -51,7 +51,6 @@ public class PurchaseOrder {
 
     /* Get the identifier for the contact on the purchase order. */
     public Long getContactId() {
-<<<<<<< HEAD
         return this.contactId;
     }
 
