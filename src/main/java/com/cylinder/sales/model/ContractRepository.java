3cd5009538bc66a851deb20724b57865b6fe2d5f
Unknown <austin.clarke.peters@gmail.com>
2017-11-14 21:20:22 -0600

    Added contract single view and changed contract list view preview

--- src/main/java/com/cylinder/sales/model/ContractRepository.java
+++ src/main/java/com/cylinder/sales/model/ContractRepository.java
@@ -3,6 +3,10 @@ package com.cylinder.sales.model;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 
 public interface ContractRepository extends CrudRepository<Contract, Long> {
 }
