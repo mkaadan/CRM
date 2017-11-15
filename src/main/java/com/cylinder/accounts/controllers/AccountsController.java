cfa5e982d37b680a9697057b576bfac62b9e2aaa
Unknown <ehm011@mail.usask.ca>
2017-11-15 12:00:12 -0600

    Add partial create new account

--- src/main/java/com/cylinder/accounts/controllers/AccountsController.java
+++ src/main/java/com/cylinder/accounts/controllers/AccountsController.java
@@ -3,13 +3,18 @@ package com.cylinder.accounts.controllers;
 import java.lang.Iterable;
 
 import com.cylinder.accounts.model.Account;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 import com.cylinder.accounts.model.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/account")
@@ -17,6 +22,8 @@ public class AccountsController {
 
       @Autowired
       private AccountRepository accountRepository;
+      @Autowired
+      private TypeRepository typeRepository;
 
     	@RequestMapping(method=RequestMethod.GET)
       // Render the list view for Accounts.
@@ -34,5 +41,74 @@ public class AccountsController {
         model.addAttribute("accountData", accountData);
         model.addAttribute("toList", "/");
         return "accounts/singleaccount";
+    }
+
+    @RequestMapping(value="/edit/{id}", method=RequestMethod.GET)
+    public String editRecord(@ModelAttribute("accountData") Account account,
+                             @PathVariable("id") Long id,
+                             Model model) {
+        if (accountRepository.existsById(id)) {
+            account = accountRepository.findOne(id);
+        } else {
+            return "redirect:/errors";
+        }
+        Iterable<Account> accountData =  accountRepository.findAll();
+        model.addAttribute("accountData", accountData);
+        Iterable<Account> userData =  .findAll();
+        model.addAttribute("accountData", accountData);
+        Iterable<Type> typeData = typeRepository.findAll();
+        model.addAttribute("accountType", typeData);
+        model.addAttribute("moduleName", "Accounts");
+        model.addAttribute("accountData", account);
+        model.addAttribute("toList", "/account");
+        return "accounts/editsingle";
+    }
+
+    @RequestMapping(value="/new/", method=RequestMethod.GET)
+    public String newRecord(@ModelAttribute("accountData") Account account,
+                            Model model) {
+        Iterable<Type> typeData = typeRepository.findAll();
+        model.addAttribute("accountType", typeData);
+        model.addAttribute("moduleName", "Accounts");
+        model.addAttribute("accountData", new Account());
+        model.addAttribute("toList", "/account");
+        return "accounts/editsingle";
+    }
+
+    @RequestMapping(value="/records/{id}", method=RequestMethod.DELETE)
+    @ResponseBody
+    public String deleteRecord(@PathVariable("id") Long id) {
+        if (accountRepository.existsById(id)) {
+            accountRepository.deleteById(id);
+            return "";
+        } else {
+            return "Failed to delete record" + id;
+        }
+    }
+
+    @RequestMapping(value="/records", method=RequestMethod.POST)
+    public String saveRecord(@Valid @ModelAttribute("accountData") Account account,
+                             BindingResult result,
+                             Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("moduleName", "Accounts");
+            Iterable<Type> typeData = typeRepository.findAll();
+            model.addAttribute("accountType", typeData);
+            model.addAttribute("toList", "/account");
+            return "accounts/editsingle";
+        }
+        if (account.getShippingAddress().areFieldsNull()) {
+            account.setShippingAddress(null);
+        }
+        if (account.getBillingAddress().areFieldsNull()) {
+            account.setBillingAddress(null);
+        }
+        Long assignedId = accountRepository.save(account).getAccountId();
+        return "redirect:/account/records/" + assignedId.toString() ;
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 }
--- src/main/java/com/cylinder/accounts/model/AccountRepository.java
+++ src/main/java/com/cylinder/accounts/model/AccountRepository.java
@@ -1,6 +1,17 @@
 package com.cylinder.accounts.model;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface AccountRepository extends CrudRepository<Account, Long> {
+    @Query(value="SELECT exists(SELECT 1 FROM account.details WHERE account_id=:accountId)", nativeQuery=true)
+    boolean existsById(@Param("accountId")Long accountId);
+
+    @Transactional
+    @Modifying
+    @Query(value="DELETE FROM account.details WHERE account_id=:accountId", nativeQuery=true)
+    int deleteById(@Param("accountId") Long accountId);
 }
