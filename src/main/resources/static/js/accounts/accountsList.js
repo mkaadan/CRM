434708f6a994ff405dbd59ffe83f665f9651341e
Unknown <ehm011@mail.usask.ca>
2017-11-16 16:57:50 -0600

    Finished new/edit views
    modified js delete functions

--- src/main/resources/static/js/accounts/accountsList.js
+++ src/main/resources/static/js/accounts/accountsList.js
@@ -1,20 +1,24 @@
-$(document).ready(function() {
-    var table = $('#accountList').DataTable( {
-        scrollY:        '50vh',
+$(document).ready(function () {
+    var table = $('#accountList').DataTable({
+        scrollY: '50vh',
         scrollCollapse: true,
-        paging:         false
-    } );
-} );
+        paging: false
+    });
+});
 
-$(document).ready(function() {
-    $(".glyphicon-trash").click(function(event) {
+$(document).ready(function () {
+    $(".glyphicon-trash").click(function (event) {
         var thisRecord = $(this);
+        var csrfToken = $("meta[name='_csrf']").attr("content");
         if (confirm("Are you sure you wish to delete this record?")) {
             var id = thisRecord.data("id");
             $.ajax({
                 url: '/account/records/' + id,
                 type: 'DELETE',
-                success: function(result) {
+                headers: {
+                    "X-CSRF-TOKEN": csrfToken,
+                },
+                success: function (result) {
                     var table = $('#accountList').DataTable();
                     var row = thisRecord.parents('tr');
                     table.row(row)
--- src/main/resources/static/js/accounts/edit.js
+++ src/main/resources/static/js/accounts/edit.js
@@ -1,16 +1,20 @@
-$(document).ready(function() {
-    $("#deleteRecord").click(function(event) {
+$(document).ready(function () {
+    $("#deleteRecord").click(function (event) {
         var thisRecord = $(this);
+        var csrfToken = $("meta[name='_csrf']").attr("content");
         if (confirm("Are you sure you wish to delete this record?")) {
-          var id = thisRecord.data("id");
-          $.ajax({
-                  url: '/account/records/' + id,
-                  type: 'DELETE',
-                  success: function(result) {
+            var id = thisRecord.data("id");
+            $.ajax({
+                url: '/account/records/' + id,
+                type: 'DELETE',
+                headers: {
+                    "X-CSRF-TOKEN": csrfToken,
+                },
+                success: function (result) {
                     var hostName = window.location.hostname;
                     window.location.replace("/account/");
-                  }
-              });
+                }
+            });
         }
     });
 });
