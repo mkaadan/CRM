b4df255de78ef6f74eec9f5d583dc9c670782166
rpiper <rpiper@rpipercomp.gateway.sasktel.net>
2017-11-15 21:11:31 -0600

    Add comments; update controller logic

--- src/main/java/com/cylinder/leads/controllers/LeadsController.java
+++ src/main/java/com/cylinder/leads/controllers/LeadsController.java
@@ -11,67 +11,143 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletResponse;
 
 import javax.validation.Valid;
 
 import com.cylinder.leads.model.*;
+import com.cylinder.shared.BaseController;
+import com.cylinder.crmusers.model.CrmUserRepository;
+import com.cylinder.crmusers.model.CrmUser;
+
 
 @Controller
 @RequestMapping("/lead")
-public class LeadsController {
+public class LeadsController extends BaseController {
 
+      /**
+      * Sql interface for lead entites.
+      */
       @Autowired
       private LeadRepository leadRepository;
 
+      /**
+      * Sql interface for lead status entites.
+      */
       @Autowired
       private StatusRepository statusRespository;
 
+      /**
+      * Sql interface for source entites.
+      */
       @Autowired
       private SourceRepository sourceRespository;
 
-    	@RequestMapping(method=RequestMethod.GET)
-      // Render the list view for leads.
-    	public String list(Model model){
-        model.addAttribute("moduleName", "Leads");
+      /**
+      * Sql interface for crm user entites.
+      */
+      @Autowired
+      private CrmUserRepository userRepository;
+
+      /**
+      * The module name that this controller is associated to.
+      */
+      private final String moduleName = "Leads";
+
+      /**
+      * Render the list view for all available leads.
+      * @param model the view model object that is used to render the html.
+      * @param auth the authentication context that manages which users are logged in.
+      * @return the name of the template to render.
+      */
+    	@GetMapping
+    	public String list(Model model, Authentication auth){
         Iterable<Lead> leadData =  leadRepository.findAll();
+        super.setCommonModelAttributes(model, auth, userRepository, this.moduleName);
         model.addAttribute("leadData", leadData);
         return "leads/list";
       }
 
-      @RequestMapping(value="/records/{id}", method=RequestMethod.GET)
-      public String singleRecord(@PathVariable("id") Long id, Model model) {
+      /**
+      * Render the list view for all available leads.
+      * @param model the view model object that is used to render the html.
+      * @param auth the authentication context that manages which users are logged in.
+      * @return the name of the template to render.
+      */
+      @GetMapping("/records/{id}")
+      public String singleRecord(@PathVariable("id") Long id,
+                                 Model model,
+                                 Authentication auth) {
           Lead leadData = leadRepository.findOne(id);
-          model.addAttribute("moduleName", "Leads");
+          super.setCommonModelAttributes(model,auth,userRepository, this.moduleName);
           model.addAttribute("leadData", leadData);
           model.addAttribute("toList", "/lead");
           return "leads/singlelead";
       }
 
-      @RequestMapping(value="/edit/{id}", method=RequestMethod.GET)
-      public String editRecord(@ModelAttribute("leadData") Lead lead,
-                               @PathVariable("id") Long id,
-                               Model model) {
+      /**
+      * Render a edit view for a single lead.
+      * @param id the id that is associated to some lead.
+      * @param model the view model object that is used to render the html.
+      * @param auth the authentication context that manages which users are logged in.
+      * @return the name of the template to render.
+      */
+      @GetMapping(value="/edit/{id}")
+      public String editLead(@PathVariable("id") Long id,
+                               Model model,
+                               Authentication auth,
+                               HttpServletResponse response) {
+          Lead lead;
           if (leadRepository.existsById(id)) {
                lead = leadRepository.findOne(id);
           } else {
-               return "redirect:/errors";
+               response.setStatus(404);
+               return "redirect:/404.html";
           }
           Iterable<Source> sourceData = sourceRespository.findAll();
           Iterable<Status> statusData = statusRespository.findAll();
-          model.addAttribute("moduleName", "Leads");
+          super.setCommonModelAttributes(model,auth,userRepository, this.moduleName);
+          model.addAttribute("userData", userRepository.findAll());
           model.addAttribute("leadStatus", statusData);
           model.addAttribute("leadSource", sourceData);
           model.addAttribute("leadData", lead);
           model.addAttribute("toList", "/lead");
+          model.addAttribute("action", "edit/" + id);
           return "leads/editsingle";
       }
 
-      @RequestMapping(value="/new/", method=RequestMethod.GET)
-      public String newRecord(@ModelAttribute("leadData") Lead lead,
-                              Model model) {
+      /**
+      * Process a form for editing a single record.
+      * @param id the id that is associated to some lead.
+      * @param model the view model object that is used to render the html.
+      * @param auth the authentication context that manages which users are logged in.
+      * @return the name of the template to render.
+      */
+      @PostMapping(value="/edit/{id}")
+      public String saveEditableLead(@PathVariable("id") Long id,
+                             @Valid @ModelAttribute("leadData") Lead lead,
+                             BindingResult result,
+                             Model model,
+                             Authentication auth) {
+        return this.saveNewLead(lead, result, model, auth);
+      }
+
+      /**
+      * Render a view for a creating a single lead.
+      * @param model the view model object that is used to render the html.
+      * @param auth the authentication context that manages which users are logged in.
+      * @return the name of the template to render.
+      */
+      @GetMapping("/new/")
+      public String newRecord(Model model,
+                              Authentication auth) {
           Iterable<Source> sourceData = sourceRespository.findAll();
           Iterable<Status> statusData = statusRespository.findAll();
-          model.addAttribute("moduleName", "Leads");
+          super.setCommonModelAttributes(model,auth,userRepository, this.moduleName);
+          model.addAttribute("action","new/");
+          model.addAttribute("userData", userRepository.findAll());
           model.addAttribute("leadStatus", statusData);
           model.addAttribute("leadSource", sourceData);
           model.addAttribute("leadData", new Lead());
@@ -79,7 +155,53 @@ public class LeadsController {
           return "leads/editsingle";
       }
 
-      @RequestMapping(value="/records/{id}", method=RequestMethod.DELETE)
+      /**
+      * Process a new lead form and potentially send errors back.
+      * @param lead The lead form object to be processed.
+      * @param result the object that binds the data from the view and validates user.
+      * @param model the view model object that is used to render the html.
+      * @param auth the authentication context that manages which users are logged in.
+      * @return the name of the template to render.
+      */
+      @PostMapping("/new/")
+      public String saveNewLead(@Valid @ModelAttribute("leadData") Lead lead,
+                                BindingResult result,
+                                Model model,
+                                Authentication auth) {
+        if (result.hasErrors()) {
+            Iterable<Source> sourceData = sourceRespository.findAll();
+            Iterable<Status> statusData = statusRespository.findAll();
+            super.setCommonModelAttributes(model,auth,userRepository,this.moduleName);
+            model.addAttribute("userData", userRepository.findAll());
+            model.addAttribute("leadStatus", statusData);
+            model.addAttribute("leadSource", sourceData);
+            model.addAttribute("toList", "/lead");
+            if (lead.getLeadId() == null) {
+              model.addAttribute("action","new/");
+            } else {
+              model.addAttribute("action","/edit/" + lead.getLeadId());
+            }
+            return "leads/editsingle";
+        }
+        if (lead.getAddress().areFieldsNull()) {
+          lead.setAddress(null);
+        }
+        CrmUser user = userRepository.findByEmail(auth.getName());
+        lead.setLastModifiedBy(user);
+        if (lead.getLeadId() == null) {
+          lead.setCreatedBy(user);
+        }
+        Long assignedId = leadRepository.save(lead).getLeadId();
+        return "redirect:/lead/records/" + assignedId.toString() ;
+      }
+
+
+      /**
+      * Delete some lead through a delete request.
+      * @param id the id that is associated to some lead.
+      * @return the name of the template to render.
+      */
+      @DeleteMapping("/records/{id}")
       @ResponseBody
       public String deleteRecord(@PathVariable("id") Long id) {
         if (leadRepository.existsById(id)) {
@@ -91,26 +213,11 @@ public class LeadsController {
 
       }
 
-      @RequestMapping(value="/records", method=RequestMethod.POST)
-      public String saveRecord(@Valid @ModelAttribute("leadData") Lead lead,
-                               BindingResult result,
-                               Model model) {
-        if (result.hasErrors()) {
-            model.addAttribute("moduleName", "Leads");
-            Iterable<Source> sourceData = sourceRespository.findAll();
-            Iterable<Status> statusData = statusRespository.findAll();
-            model.addAttribute("leadStatus", statusData);
-            model.addAttribute("leadSource", sourceData);
-            model.addAttribute("toList", "/lead");
-            return "leads/editsingle";
-        }
-        if (lead.getAddress().areFieldsNull()) {
-          lead.setAddress(null);
-        }
-        Long assignedId = leadRepository.save(lead).getLeadId();
-        return "redirect:/lead/records/" + assignedId.toString() ;
-      }
 
+      /**
+      * Maps empty string to null when a form is submitted.
+      * @param binder The object that allows for empty strings to be turned into nulls.
+      */
       @InitBinder
       public void initBinder(WebDataBinder binder) {
         binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
