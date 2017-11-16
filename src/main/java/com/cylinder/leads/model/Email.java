fd9699a04e935134bf6f31d72471a1820dc8633c
rpiper <rpiper@rpipercomp.gateway.sasktel.net>
2017-11-15 22:58:28 -0600

    Add comments

--- src/main/java/com/cylinder/leads/model/Email.java
+++ src/main/java/com/cylinder/leads/model/Email.java
@@ -4,6 +4,10 @@ package com.cylinder.leads.model;
 import javax.persistence.*;
 import com.cylinder.shared.SimpleEmail;
 
+/**
+* @author Ryan Piper
+* The table mapping for a lead's email .
+*/
 @Entity
 @Table(name="emails", schema="lead")
 public class Email extends SimpleEmail {
--- src/main/java/com/cylinder/leads/model/EmailRepository.java
+++ src/main/java/com/cylinder/leads/model/EmailRepository.java
@@ -4,5 +4,9 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+* @author Ryan Piper
+* Sql interface for a lead's email. 
+*/
 public interface EmailRepository extends CrudRepository<Email, Long> {
 }
