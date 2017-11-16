71adf48509d0b28f2dc1b7d3ebc1ab71c35118dd
rpiper <rpiper@rpipercomp.gateway.sasktel.net>
2017-11-16 16:33:20 -0600

    Add comments

--- src/main/java/com/cylinder/leads/model/SourceRepository.java
+++ src/main/java/com/cylinder/leads/model/SourceRepository.java
@@ -2,5 +2,9 @@ package com.cylinder.leads.model;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+* @author Ryan Piper
+* Sql Interface for lead sources
+*/
 public interface SourceRepository extends CrudRepository<Source, Long> {
 }
