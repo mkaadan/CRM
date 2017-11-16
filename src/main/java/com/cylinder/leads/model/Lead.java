0a84468788eece5b1bb55dd164fd6c8caae88c15
rpiper <rpiper@rpipercomp.gateway.sasktel.net>
2017-11-16 16:32:48 -0600

    Remove audit information and inherit a class that manages it.

--- src/main/java/com/cylinder/leads/model/Lead.java
+++ src/main/java/com/cylinder/leads/model/Lead.java
@@ -13,10 +13,11 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.*;
+import com.cylinder.shared.model.SimpleAudit;
 
 @Entity
 @Table(name="details", schema="lead")
-public class Lead {
+public class Lead extends SimpleAudit {
 
     /**
     * The identifier for a lead.
@@ -198,50 +199,6 @@ public class Lead {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="owner_id", referencedColumnName = "account_id")
     protected CrmUser owner;
-
-    /**
-    * Get the user that last created this lead.
-    *
-    * @param createdBy the creator of the lead.
-    * @return the creator of the lead.
-    */
-    @Getter
-    @Setter
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name="created_by", referencedColumnName = "account_id", updatable=false)
-    protected CrmUser createdBy;
-
-    /**
-    * Get the user that last modified this lead.
-    *
-    * @param owner the new owner of the lead.
-    * @return the owner of the lead.
-    */
-    @Getter
-    @Setter
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name="last_modified_by_id", referencedColumnName = "account_id")
-    protected CrmUser lastModifiedBy;
-
-    /**
-    * The time that this record was created.
-    *
-    * @return the timestamp of when this lead was added.
-    */
-    @Getter
-    @CreationTimestamp
-    @Column(updatable=false)
-    protected Timestamp created;
-
-    /**
-    * The time that this record was last modified.
-    *
-    * @return the timestamp of when this lead was last modified.
-    */
-    @Getter
-    @UpdateTimestamp
-    @Column(name = "last_modified")
-    protected Timestamp lastModified;
 
     public Lead() {}
 
