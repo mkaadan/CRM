4ba1a273e342c1e6e2cbf6d07297befeb978b846
Unknown <ehm011@mail.usask.ca>
2017-11-15 19:11:09 -0600

    Add delete/edit in list view

--- src/main/resources/static/js/accounts/accountsList.js
+++ src/main/resources/static/js/accounts/accountsList.js
@@ -5,3 +5,23 @@ $(document).ready(function() {
         paging:         false
     } );
 } );
+
+$(document).ready(function() {
+    $(".glyphicon-trash").click(function(event) {
+        var thisRecord = $(this);
+        if (confirm("Are you sure you wish to delete this record?")) {
+            var id = thisRecord.data("id");
+            $.ajax({
+                url: '/account/records/' + id,
+                type: 'DELETE',
+                success: function(result) {
+                    var table = $('#accountList').DataTable();
+                    var row = thisRecord.parents('tr');
+                    table.row(row)
+                        .remove()
+                        .draw();
+                }
+            });
+        }
+    });
+});
