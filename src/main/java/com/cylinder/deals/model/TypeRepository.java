29886cfd20048692feae41111cce5b5454f89284
rpiper <rpiper@rpipercomp.gateway.sasktel.net>
2017-11-17 15:50:32 -0600

    Add repository annotation

--- src/main/java/com/cylinder/deals/model/TypeRepository.java
+++ src/main/java/com/cylinder/deals/model/TypeRepository.java
@@ -1,10 +1,12 @@
 package com.cylinder.deals.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
 * @author Ryan Piper
 * Sql Interface for the deal stage.
 */
+@Repository
 public interface TypeRepository extends CrudRepository<Type, Long> {
 }
