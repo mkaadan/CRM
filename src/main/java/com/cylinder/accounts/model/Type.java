aff8edb57abfd3c8635b48335c810eaf0f8f3e96
Unknown <ehm011@mail.usask.ca>
2017-11-16 18:22:37 -0600

    Add lombok annotations

--- src/main/java/com/cylinder/accounts/model/Type.java
+++ src/main/java/com/cylinder/accounts/model/Type.java
@@ -1,23 +1,23 @@
 package com.cylinder.accounts.model;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name="type", schema="account")
 public class Type {
+    @Getter
+    @Setter
     @Id
     @Column(name="type_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Long typdId;
+    protected Long typeId;
+
+    @Getter
+    @Setter
     @Column
     protected String descriptor;
-
-    public Long getTypeId() {
-        return this.typdId;
-    }
-
-    public String getDescriptor() {
-        return this.descriptor;
-    }
 }
