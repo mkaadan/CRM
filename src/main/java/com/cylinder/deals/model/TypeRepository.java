44d132a140ff89410baba5ad58dfe076f7e0d6d2
rpiper <rpiper@rpipercomp.gateway.sasktel.net>
2017-11-17 12:08:49 -0600

    Add basic deal logic

--- src/main/java/com/cylinder/deals/model/TypeRepository.java
+++ src/main/java/com/cylinder/deals/model/TypeRepository.java
@@ -1,12 +1,10 @@
 package com.cylinder.deals.model;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 /**
 * @author Ryan Piper
 * Sql Interface for the deal stage.
 */
-@Repository
 public interface TypeRepository extends CrudRepository<Type, Long> {
 }
