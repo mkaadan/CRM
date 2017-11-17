498a747d13815c8b2ee4ca469eb4feb7585a1b19
Unknown <austin.clarke.peters@gmail.com>
2017-11-16 21:15:39 -0600

    add lombok annotations

--- src/main/java/com/cylinder/sales/model/Contract.java
+++ src/main/java/com/cylinder/sales/model/Contract.java
@@ -4,36 +4,32 @@ import java.util.List;
 
 import javax.persistence.*;
 import javax.validation.constraints.*;
+import lombok.*;
 
 @Entity
 @Table(name = "contracts", schema = "sale")
 public class Contract {
     @Id
+    @Getter
+    @Setter
     @Column(name="contract_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long contractId;
 
     /** The contract's title. */
+    @Getter
+    @Setter
     @Column(name="contract_title")
     @Pattern(regexp="[a-zA-Z]{1,250}", message="Please provide a valid title.")
     private String contractTitle;
 
     /** The contract's body text. */
+    @Getter
+    @Setter
     @Column(name="contract")
     private String contractText;
 
     public Contract() { }
-
-    /* Get the identifier for the contract. */
-    public long getContractId() {
-        return this.contractId;
-    }
-
-    /* Get the title of the contract. */
-    public String getContractTitle() { return this.contractTitle; }
-
-    /* Get the body text of the contract. */
-    public String getContractText() { return this.contractText; }
 
     /* Get the preview of the body text of the contract. */
     public String getContractPreviewText() {
@@ -43,20 +39,5 @@ public class Contract {
             return contractPreviewText;
         }
         else{return this.contractText;}
-    }
-
-    /* Set the identifier for the lead. */
-    public void setContractId(long contractId) {
-        this.contractId = contractId;
-    }
-
-    /* Set the title of the contract. */
-    public void setContractTitle(String contractTitle) {
-        this.contractTitle = contractTitle;
-    }
-
-    /* Set the body text of the contract. */
-    public void setContractText(String contractText) {
-        this.contractText = contractText;
     }
 }
--- src/main/java/com/cylinder/sales/model/SalesOrder.java
+++ src/main/java/com/cylinder/sales/model/SalesOrder.java
@@ -10,176 +10,100 @@ import java.util.Set;
 @Table(name = "sales_orders", schema = "sale")
 public class SalesOrder {
     @Id
+    @Getter
+    @Setter
     @Column(name="sales_order_id")
     private Long salesOrderId;
 
     /* Billing address information associated to the sales order. */
+    @Getter
+    @Setter
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="billing_address_id", referencedColumnName="address_id")
     private Address billingAddress;
 
     /* Shipping address information associated to the sales order. */
+    @Getter
+    @Setter
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="shipping_address_id", referencedColumnName="address_id")
     private Address shippingAddress;
 
     /** The identifyer of the contact associated with the sales order. */
+    @Getter
+    @Setter
     @Column(name="contact_id")
     private Long contactId;
 
     /** The account associated with the sales order. */
+    @Getter
+    @Setter
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "account_id", referencedColumnName = "account_id")
     private Account account;
 
     /** The quote associated with the sales order. */
+    @Getter
+    @Setter
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "quote_id", referencedColumnName = "quote_id")
     private Quote quote;
 
     /** The tax ammount associated with the sales order. */
+    @Getter
+    @Setter
     @Column(name="tax_percent")
     private float taxPercent;
 
     /** The invoice number associated with the sales order. */
+    @Getter
+    @Setter
     @Column(name="invoice_number")
     private Long invoiceNumber;
 
     /** The contract associated with the sales order. */
+    @Getter
+    @Setter
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "contract_id", referencedColumnName = "contract_id")
     private Contract contract;
 
     /** The owner associated with the sales order. */
+    @Getter
+    @Setter
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="owner_id", referencedColumnName="account_id")
     private CrmUser owner;
 
     /** The identifyer of the creator of the sales order. */
+    @Getter
+    @Setter
     @Column(name="created_by")
     private Long createdBy;
 
     /** The time stamp of when the sales order was created. */
+    @Getter
+    @Setter
     @Column(name="created")
     private Timestamp created;
 
     /** The time stamp of when the sales order was last modified. */
+    @Getter
+    @Setter
     @Column(name="last_modified")
     private Timestamp lastModified;
 
     /** The identifyer of who last modified the sales order. */
+    @Getter
+    @Setter
     @Column(name="last_modified_by_id")
     private Long lastModifiedById;
 
     /** The set of products for the sales order. */
+    @Getter
+    @Setter
     @OneToMany(mappedBy = "salesOrder")
     private Set<ProductSalesOrder> productSalesOrder;
 
     public SalesOrder() { }
-
-    /* Get the identifier for the sales order. */
-    public Long getSalesOrderId() { return this.salesOrderId; }
-
-    /* Get for the quote on the sales order. */
-    public Quote getQuote() { return this.quote; }
-
-    /* Get the tax percent ammount for the sales order. */
-    public float getTaxPercent() { return this.taxPercent; }
-
-    /* Get the the invoice number on the sales order. */
-    public Long getInvoiceNumber() { return this.invoiceNumber; }
-
-    /* Get the contract on the sales order. */
-    public Contract getContract() { return this.contract; }
-
-    /* Get the owner of the sales order. */
-    public CrmUser getOwner() { return this.owner; }
-
-    /* Get the identifier for the creator of the sales order. */
-    public Long getCreatedBy() {
-        return this.createdBy;
-    }
-
-    /* Get the time stamp of when the sales order was created. */
-    public Timestamp getCreated() {
-        return this.created;
-    }
-
-    /* Get the time stamp of when the sales order was last modified. */
-    public Timestamp getLastModified() { return this.lastModified; }
-
-    /* Get the identifier for the person who last modified the sales order. */
-    public Long getLastModifiedById() { return this.lastModifiedById; }
-
-    /* Get the billing address information the sales order is associated with. */
-    public Address getBillingAddress() { return this.billingAddress; }
-
-    /* Get the shipping address information the sales order is associated with. */
-    public Address getShippingAddress() { return this.shippingAddress; }
-
-    /* Get the identifier for the contact on the sales order. */
-    public Long getContactId() { return this.contactId; }
-
-    /* Get the account on the sales order. */
-    public Account getAccount() { return this.account; }
-
-    /** Get the set of products for the quote */
-    public Set<ProductSalesOrder> getProductSalesOrder() {
-        return productSalesOrder;
-    }
-
-    /* Set the identifier for the sales order. */
-    public void setSalesOrderId(Long salesOrderId) { this.salesOrderId = salesOrderId; }
-
-    /* Set the quote on the sales order. */
-    public void setQuote(Quote quote) { this.quote = quote; }
-
-    /* Set the tax percent ammount for the sales order. */
-    public void setTaxPercent(float taxPercent) { this.taxPercent = taxPercent; }
-
-    /* Set the the invoice number on the sales order. */
-    public void setInvoiceNumber(Long invoiceNumber) { this.invoiceNumber = invoiceNumber; }
-
-    /* Set the contract on the sales order. */
-    public void setContract(Contract contract) { this.contract = contract; }
-
-    /* Set the owner of the sales order. */
-    public void setOwner(CrmUser owner) { this.owner = owner; }
-
-    /* Set the identifier for the creator of the sales order. */
-    public void setCreatedBy(Long createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    /* Set the time stamp of when the sales order was created. */
-    public void setCreated(Timestamp created) {
-        this.created = created;
-    }
-
-    /* Set the time stamp of when the sales order was last modified. */
-    public void setLastModified(Timestamp lastModified) {
-        this.lastModified = lastModified;
-    }
-
-    /* Set the identifier for the person who last modified the sales order. */
-    public void setLastModifiedById(Long lastModifiedById) {
-        this.lastModifiedById = lastModifiedById;
-    }
-
-    /* Set the billing address information the sales order is associated with. */
-    public void setBillingAddress(Address billingAddress) { this.billingAddress = billingAddress; }
-
-    /* Set the shipping address information the sales order is associated with. */
-    public void setShippingAddress(Address shippingAddress) { this.shippingAddress = shippingAddress; }
-
-    /* Set the identifier for the contact on the sales order. */
-    public void setContactId(Long contactId) { this.contactId = contactId; }
-
-    /* Set the account on the sales order. */
-    public void setAccount(Account account) { this.account = account; }
-
-    /** Set the set of products for the sales order */
-    public void setProductSalesOrder(Set<ProductSalesOrder> productSalesOrder) {
-        this.productSalesOrder = productSalesOrder;
-    }
 }
