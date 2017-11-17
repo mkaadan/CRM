8713338a4547f9be3f2de3e4a604494818567d90
Unknown <austin.clarke.peters@gmail.com>
2017-11-17 11:22:30 -0600

    Changed QuotesController to extend BaseController

--- src/main/java/com/cylinder/sales/controllers/QuotesController.java
+++ src/main/java/com/cylinder/sales/controllers/QuotesController.java
@@ -2,38 +2,77 @@ package com.cylinder.sales.controllers;
 
 import java.lang.Iterable;
 
-import com.cylinder.sales.model.Quote;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.security.core.Authentication;
 
-
+import com.cylinder.shared.controllers.BaseController;
+import com.cylinder.crmusers.model.CrmUserRepository;
+import com.cylinder.crmusers.model.CrmUser;
 import com.cylinder.sales.model.*;
+
 
 @Controller
 @RequestMapping("/quote")
-public class QuotesController {
+public class QuotesController extends BaseController{
 
-      @Autowired
-      private QuoteRepository quoteRepository;
+    /**
+     * Sql interface for quote entites.
+     */
+    @Autowired
+    private QuoteRepository quoteRepository;
 
-      @Autowired
-      private ProductQuoteRepository productQuoteRepository;
+    /**
+     * Sql interface for productQuote entites.
+     */
+    @Autowired
+    private ProductQuoteRepository productQuoteRepository;
 
-    	@RequestMapping(method=RequestMethod.GET)
-      // Render the list view for Quotes.
-    	public String list(Model model){
-        model.addAttribute("moduleName", "Quotes");
+    /**
+     * Sql interface for crm user entites.
+     */
+    @Autowired
+    private CrmUserRepository userRepository;
+
+    /**
+     * The module name that this controller is associated to.
+     */
+    private final String moduleName = "Quotes";
+
+    /**
+     * Render the list view for all available quotes.
+     * @param model the view model object that is used to render the html.
+     * @param auth the authentication context that manages which users are logged in.
+     * @return the name of the template to render.
+     */
+    @GetMapping
+    public String list(Model model, Authentication auth){
         Iterable<Quote> quoteData =  quoteRepository.findAll();
+        super.setCommonModelAttributes(model, auth, userRepository, this.moduleName);
         model.addAttribute("quoteData", quoteData);
         return "sales/quotelist";
-      }
+    }
 
-    @RequestMapping(value="/records/{id}", method=RequestMethod.GET)
-    public String singleRecord(@PathVariable("id") Long id, Model model) {
+    /**
+     * Render a single view for a single quote.
+     * @param id the id that is associated to some lead.
+     * @param model the view model object that is used to render the html.
+     * @param auth the authentication context that manages which users are logged in.
+     * @return the name of the template to render.
+     */
+    @GetMapping("/records/{id}")
+    public String singleRecord(@PathVariable("id") Long id,
+                               Model model,
+                               Authentication auth) {
         Quote quoteData = quoteRepository.findOne(id);
-        model.addAttribute("moduleName", "Quotes");
+        super.setCommonModelAttributes(model,auth,userRepository, this.moduleName);
         model.addAttribute("quoteData", quoteData);
         model.addAttribute("toList", "/quote");
         Iterable<ProductQuote> productQuoteData = findAllProductsForQuote(quoteData);
@@ -41,7 +80,11 @@ public class QuotesController {
         return "sales/singlequote";
     }
 
-    //Find all the products for a single quote
+    /**
+     * Find all the products for a single quote
+     * @param quoteData the quote that is associated with the desired productquotes.
+     * @return the iterable set of productquotes.
+     */
     private Iterable<ProductQuote> findAllProductsForQuote(Quote quoteData){
         Iterable<ProductQuote> productQuoteData =  productQuoteRepository.findAll();
         for(ProductQuote productQuote : productQuoteData){
