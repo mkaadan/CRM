f74ed54064af78fe473d266dafb7b01fd8caa538
rpiper <rpiper@ppc4346b27f839.usask.ca>
2017-11-19 18:51:35 -0600

    Corrected contact mappings

--- src/main/java/com/cylinder/contacts/model/Contact.java
+++ src/main/java/com/cylinder/contacts/model/Contact.java
@@ -1,100 +1,269 @@
 package com.cylinder.contacts.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import java.sql.Date;
 
+import javax.persistence.*;
 import javax.validation.constraints.*;
 
-@MappedSuperclass
-abstract public class Contact {
+import lombok.*;
+import com.cylinder.shared.model.SimpleAudit;
+import com.cylinder.crmusers.model.CrmUser;
+import com.cylinder.accounts.model.Account; 
+
+/**
+* @author Ryan Piper
+* The information for some contact.
+*/
+@Entity
+@Table(name="details", schema="contact")
+public class Contact extends SimpleAudit {
+
+    /**
+    * The identifier for a contact.
+    *
+    * @param contactId the new contact id value.
+    * @return the id of the contact.
+    */
+    @Getter
+    @Setter
     @Id
+    @Column(name="contact_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "person_id")
-    protected Long personId;
-    @Column(name = "title_id")
-    protected Long titleId;
-    @Column(name = "company_id")
-    protected Long companyId;
-    @Column(name = "phone_id")
-    protected Long phoneId;
-    @Column(name = "address_id")
-    protected Long addressId;
-    @Column(name = "comments_id")
-    protected Long commentsId;
-    @Column(name = "contacts_id")
-    protected Long contactsId;
-    @Column(name = "user_id")
-    protected Long userId;
+    private Long contactId;
 
-    public Long getPersonId() {
-        return this.personId;
+    /**
+    * The first name of the lead.
+    *
+    * @param firstName the new first name of the lead.
+    * @return the first name of the lead.
+    */
+    @Getter
+    @Setter
+    @Column(name="first_name")
+    @Pattern(regexp="([a-zA-Z]{1,250})", message="Please enter a valid first name.")
+    private String firstName;
+
+    /**
+    * The last name of the lead.
+    *
+    * @param lastName the new last name of the lead.
+    * @return the last name of the lead.
+    */
+    @Getter
+    @Setter
+    @Column(name="last_name")
+    @NotNull(message="Please provide the last name for the lead.")
+    @Pattern(regexp="[a-zA-Z]{1,250}", message="Please provide a valid last name.")
+    private String lastName;
+
+    /**
+    * The title or the position the lead retains.
+    *
+    * @param title the new title for the lead.
+    * @return the title or position of the lead.
+    */
+    @Getter
+    @Setter
+    @Column
+    @Pattern(regexp="[a-zA-Z]{1,250}", message="Please provide a valid title.")
+    private String title;
+
+    /**
+    * The department the contact is in.
+    *
+    * @param department the new department for the contact.
+    * @return the department of the contact.
+    */
+    @Getter
+    @Setter
+    @Column
+    @Pattern(regexp="[a-zA-Z\\s0-9]{1,250}", message="Please provide a valid department.")
+    private String department;
+
+    /**
+    * The account/buisness the contact is associated with.
+    *
+    * @param account the new account value.
+    * @return the account the contact is associated with.
+    */
+    @Getter
+    @Setter
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "account_id", referencedColumnName = "account_id")
+    private Account account;
+
+    /**
+    * The phone number of the contact.
+    *
+    * @param phoneNumber the new phone number value.
+    * @return the phone number for the lead.
+    */
+    @Getter
+    @Setter
+    @Column
+    private String phone;
+
+    /**
+    * The alternative phone number of the contact.
+    *
+    * @param otherPhone the new phone number value.
+    * @return the alternative phone number for the contact.
+    */
+    @Getter
+    @Setter
+    @Column(name="other_phone")
+    private String otherPhone;
+
+
+    /**
+    * The mobile number of the contact.
+    *
+    * @param mobile the new mobile value.
+    * @return the mobile number for the contact.
+    */
+    @Getter
+    @Setter
+    @Column
+    private String mobile;
+
+    /**
+    * The fax number of the contact.
+    *
+    * @param fax the new fax value.
+    * @return the fax number for the contact.
+    */
+    @Getter
+    @Setter
+    @Column
+    private String fax;
+
+    /**
+    * The birthday of the contact.
+    *
+    * @param dateOfBirth the new date of birth value.
+    * @return the date of birth.
+    */
+    private Date dateOfBirth;
+
+    /**
+    * The assistant to the contact.
+    *
+    * @param assistant the new assistant value for the contact.
+    * @return the contact's assistant.
+    */
+    @Getter
+    @Setter
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="assistant_id", referencedColumnName="contact_id")
+    private Contact assistant;
+
+    /**
+    * The contact's boss.
+    *
+    * @param reportsTo the new report to value for the contact.
+    * @return the contact's boss.
+    */
+    @Getter
+    @Setter
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="report_to_id", referencedColumnName="contact_id")
+    private Contact reportsTo;
+
+    /**
+    * The mailing address information associated to the contact.
+    *
+    * @param mailingAddress the address value for the contact.
+    * @return the address of the contact.
+    */
+    @Getter
+    @Setter
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="mailing_address_id", referencedColumnName="address_id")
+    private Address mailingAddress;
+
+    /**
+    * Alternative address information associated to the contact.
+    *
+    * @param otherAddress the address value for the contact.
+    * @return the address of the contact.
+    */
+    @Getter
+    @Setter
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="other_address_id", referencedColumnName="address_id")
+    private Address otherAddress;
+
+    /**
+    * The primary email the lead wants to be contacted through.
+    *
+    * @param primaryEmail the new primary email value.
+    * @return the primary email of the lead.
+    */
+    @Getter
+    @Setter
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="primary_email_id", referencedColumnName="email_id")
+    private Email  primaryEmail;
+
+    /**
+    * A alternative email if the primary email address doesn't work.
+    *
+    * @param secondaryEmail the new secondary email value.
+    * @return the secondary email for the lead.
+    */
+    @Getter
+    @Setter
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="secondary_email_id", referencedColumnName="email_id")
+    private Email  secondaryEmail;
+
+    /**
+    * The twitter handlebar the contact uses.
+    *
+    * @param twitter the new  twitter value.
+    * @return the twitter handlebar associated to the contact.
+    */
+    @Getter
+    @Setter
+    @Column
+    @Pattern(regexp="@.*", message="Please provide a valid twitter handler.")
+    private String twitter;
+
+    /**
+    * The twitter handlebar the contact uses.
+    *
+    * @param twitter the new  twitter value.
+    * @return the twitter handlebar associated to the contact.
+    */
+    @Getter
+    @Setter
+    @Column
+    @Pattern(regexp=".{1,250}", message="Please provide a valid twitter handler.")
+    private String skype;
+
+    /**
+    * Get the user that is responsible for this lead.
+    *
+    * @param owner the new owner of the lead.
+    * @return the owner of the lead.
+    */
+    @Getter
+    @Setter
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="owner_id", referencedColumnName = "account_id")
+    private CrmUser owner;
+
+    public Contact() {}
+
+    /**
+    * A shortcut method to get the first and last name of the lead.
+    * @return the full name of the lead.
+    */
+    public String getFullName() {
+      if (this.firstName == null) {
+        return this.lastName;
+      } else {
+        return this.firstName + " " + this.lastName;
+      }
     }
-
-    public Long getTitleId() {
-        return this.titleId;
-    }
-
-    public Long getCompanyId() {
-        return this.companyId;
-    }
-
-    public Long getPhoneId() {
-        return this.phoneId;
-    }
-
-    public Long getAddressId() {
-        return this.addressId;
-    }
-
-    public Long getCommentsId() {
-        return this.commentsId;
-    }
-
-    public Long getContactsId() {
-        return this.contactsId;
-    }
-
-    public Long getUserId() {
-        return this.userId;
-    }
-
-    public void setPersonId(Long personId) {
-        this.personId = personId;
-    }
-
-    public void setTitleId(Long titleId) {
-        this.titleId = titleId;
-    }
-
-    public void setCompanyId(Long companyId) {
-        this.companyId = companyId;
-    }
-
-    public void setPhoneId(Long phoneId) {
-        this.phoneId = phoneId;
-    }
-
-    public void setAddressId(Long addressId) {
-        this.addressId = addressId;
-    }
-
-    public void setCommentsId(Long commentsId) {
-        this.commentsId = commentsId;
-    }
-
-    public void setContactsId(Long contactsId) {
-        this.contactsId = contactsId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-
-
-
 }
