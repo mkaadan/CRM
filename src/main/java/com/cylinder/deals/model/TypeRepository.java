800e3689341ef5cdeaf68dc3c69cda29f5c4aa13
rpiper <rpiper@ppc4346b27f839.usask.ca>
2017-11-21 15:08:57 -0600

    Removed repository annotation.

--- src/main/java/com/cylinder/deals/model/TypeRepository.java
+++ src/main/java/com/cylinder/deals/model/TypeRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 * @author Ryan Piper
 * Sql Interface for the deal stage.
 */
-@Repository
 public interface TypeRepository extends CrudRepository<Type, Long> {
 }
