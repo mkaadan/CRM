fee89651c86a2a525bc6e0941bb3fb582e0bd4d0
Molham Kaadan <mok340@drone13.usask.ca>
2017-11-20 19:05:10 -0600

    Layout and buttons all fixed in list view

--- src/main/resources/static/js/product/edit.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  deleteRecord("#deleteRecord", "/product/records/", "/product/");
-});
--- src/main/resources/static/js/product/list.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-  readIntoTable("#productList");
-  tableDelete('/product/records/', "#productList");
-});
