49263d92b2ae9da4941201ec598341c58cb86488
Unknown <austin.clarke.peters@gmail.com>
2017-12-03 19:48:09 -0600

    Abstracted conversion functions into a service object

--- src/main/java/com/cylinder/sales/model/ListIterableServiceObject.java
+++ src/main/java/com/cylinder/sales/model/ListIterableServiceObject.java
@@ -1,6 +1,35 @@
 package com.cylinder.sales.model;
 
-public abstract class ListIterableServiceObject {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ListIterableServiceObject<T> {
+
+    /**
+     * converts a iterable object into a list
+     *
+     * @param products  the iterable to convert
+     * @return a list of all the products in the iterable
+     */
+    public static <T> List<T> iterableToList(Iterable<T> products){
+        List<T> productList = new ArrayList<T>();
+        for(T product : products){
+            productList.add(product);
+        }
+        return productList;
+    }
+
+
+    /**
+     * converts a list object into a iterable
+     *
+     * @param products  the list to convert
+     * @return an iterable of all the products in the list
+     */
+    public static <T> Iterable<T> listToIterable(List<T> products){
+        Iterable<T> productIterrable = products;
+        return productIterrable;
+    }
 
     public ListIterableServiceObject() {
     }
