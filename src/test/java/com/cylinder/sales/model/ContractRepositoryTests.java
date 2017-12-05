127cd2efd6592a3b63791c9584a8ea44c2c26319
Marcus Hamilton <mah985@mail.usask.ca>
2017-12-05 15:02:28 -0600

    Initial commit for contact tests. Currently ContractRepositoryTests is done and runs with no errors.

---
+++ src/test/java/com/cylinder/sales/model/ContractRepositoryTests.java
@@ -0,0 +1,52 @@
+package com.cylinder.sales.model.ContractRepositoryTests;
+
+import com.cylinder.sales.model.ContractRepository;
+import com.cylinder.sales.model.Contract;
+import com.cylinder.RespositoryTests;
+
+import org.junit.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+public class ContractRepositoryTests extends RespositoryTests
+{
+  @Autowired
+  ContractRepository contractRepository;
+
+  @Before
+   public void initData(){
+     Contract contract = new Contract();
+     contract.setContractTitle("ContractOne");
+     contract.setContractText("This is the body. ");
+     contractRepository.save(contract);
+     contract = new Contract();
+     contract.setContractTitle("ContractTwo");
+     contractRepository.save(contract);
+     contract.setContractTitle("ContractThree");
+     contractRepository.save(contract);
+   }
+
+   @Test
+   public void testExistsBy(){
+     Long id = new Long("1");
+     boolean isExisting = contractRepository.existsById(id);
+     assertEquals(isExisting, true);
+     id = new Long("4");
+     isExisting = contractRepository.existsById(id);
+     assertEquals(isExisting, false);
+   }
+
+   @Test
+   public void testDeleteById(){
+     Long id = new Long("4");
+     boolean isExisting = contractRepository.existsById(id);
+     assertEquals(isExisting, true);
+     contractRepository.deleteById(id);
+     isExisting = contractRepository.existsById(id);
+     assertEquals(isExisting, false);
+   }
+
+
+}
