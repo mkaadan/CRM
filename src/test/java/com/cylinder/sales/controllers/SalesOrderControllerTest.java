6bf99035f5ccf02e1710bcbddb12993e3b9a1b8a
Unknown <austin.clarke.peters@gmail.com>
2017-12-04 21:32:35 -0600

    Added more to sales order controller test and fixed label issues in SOs

--- src/test/java/com/cylinder/sales/controllers/SalesOrderControllerTest.java
+++ src/test/java/com/cylinder/sales/controllers/SalesOrderControllerTest.java
@@ -38,6 +38,12 @@ public class SalesOrderControllerTest extends ControllerTests {
   @MockBean
   private SalesOrderRepository salesOrderRepository;
 
+//  @MockBean
+//  private QuoteRepository quoteRepository;
+//
+//  @MockBean
+//  private ProductSalesOrderRepository productSalesOrderRepository;
+
   private ArrayList<SalesOrder> mockSalesOrderListData() {
     ArrayList<SalesOrder> salesOrders = new ArrayList();
     SalesOrder salesOrder = new SalesOrder();
@@ -57,4 +63,59 @@ public class SalesOrderControllerTest extends ControllerTests {
     salesOrder.setSalesOrderId(new Long("1"));
     return salesOrder;
   }
+
+//    private ArrayList<Quote> mockQuoteData() {
+//        ArrayList<Quote> quotes = new ArrayList();
+//        Quote quote = new Quote();
+//        quote.setQuoteId(new Long("1"));
+//        quotes.add(quote);
+//        quote = new Quote();
+//        quote.setQuoteId(new Long("2"));
+//        quotes.add(quote);
+//        quote = new Quote();
+//        quote.setQuoteId(new Long("3"));
+//        quotes.add(quote);
+//        return quotes;
+//    }
+//
+//    private ArrayList<ProductSalesOrder> mockProductSalesOrderData() {
+//        ArrayList<ProductSalesOrder> productSalesOrders = new ArrayList();
+//        ProductSalesOrder productSalesOrder = new ProductSalesOrder();
+//        productSalesOrder.setSalesOrderProductId(new Long("1"));
+//        productSalesOrders.add(productSalesOrder);
+//        productSalesOrder = new ProductSalesOrder();
+//        productSalesOrder.setSalesOrderProductId(new Long("2"));
+//        productSalesOrders.add(productSalesOrder);
+//        productSalesOrder = new ProductSalesOrder();
+//        productSalesOrder.setSalesOrderProductId(new Long("3"));
+//        productSalesOrders.add(productSalesOrder);
+//        return productSalesOrders;
+//    }
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(salesOrderController)
+                .apply(springSecurity(super.springSecurityFilterChain))
+                .build();
+        super.mockUserRepository();
+        Iterable<SalesOrder> salesOrders = mockSalesOrderListData();
+//        Iterable<Quote> quotes = mockQuoteData();
+//        Iterable<ProductSalesOrder> productSalesOrders = mockProductSalesOrderData();
+        given(this.salesOrderRepository.findAll()).willReturn(salesOrders);
+        given(this.salesOrderRepository.findOne(eq(new Long("1")))).willReturn(mockSingleSalesOrderData());
+        given(this.salesOrderRepository.findOne(eq(new Long("1")))).willReturn(null);
+        given(this.salesOrderRepository.existsById(new Long("1"))).willReturn(true);
+        given(this.salesOrderRepository.existsById(new Long("5"))).willReturn(false);
+        given(this.salesOrderRepository.save(any(SalesOrder.class))).willReturn(mockSingleSalesOrderData());
+//        given(this.quoteRepository.findAll()).willReturn(quotes);
+//        given(this.productSalesOrderRepository.findAll()).willReturn(productSalesOrders);
+    }
+
+    @Test
+    @WithMockUser(username="fake@mail.com", authorities="USER")
+    public void testIndex() throws Exception {
+        this.mockMvc.perform(get("/salesorder"))
+                .andExpect(status().isOk());
+    }
 }
