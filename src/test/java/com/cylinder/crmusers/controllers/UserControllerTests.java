f339876e24e7e6469e8f7555bbe3c826d13827a7
rpiper <rpiper@rpipercomp.gateway.sasktel.net>
2017-12-05 20:58:19 -0600

    Add tests and mocks.

--- src/test/java/com/cylinder/crmusers/controllers/UserControllerTests.java
+++ src/test/java/com/cylinder/crmusers/controllers/UserControllerTests.java
@@ -30,13 +30,17 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 public class UserControllerTests extends ControllerTests {
 
    @InjectMocks
    UserController userController;
+
+   @MockBean
+   BCryptPasswordEncoder passwordEncoder;
 
   @Before
   public void setup() {
@@ -45,6 +49,15 @@ public class UserControllerTests extends ControllerTests {
                              .apply(springSecurity(super.springSecurityFilterChain))
                              .build();
     super.mockUserRepository();
+    given(this.userRepository.existsByAccountId(new Long("1"))).willReturn(true);
+    given(this.userRepository.existsByAccountId(new Long("5"))).willReturn(false);
+    given(this.userRepository.existsByAccountId(new Long("2"))).willReturn(true);
+    given(this.passwordEncoder.matches(eq("p@ssw0rd"),
+                                       eq("$2a$12$pb3lrDyioOI.jb1U9ZLUMOUAzKUp4vMdxg9CoEHp0ZwjfD1o/Qf8a")))
+                              .willReturn(true);
+    given(this.passwordEncoder.matches(eq("p@ssw0rd321"),
+                                       eq("$2a$12$pb3lrDyioOI.jb1U9ZLUMOUAzKUp4vMdxg9CoEHp0ZwjfD1o/Qf8a")))
+                              .willReturn(false);
   }
 
   @Test
@@ -52,5 +65,57 @@ public class UserControllerTests extends ControllerTests {
   public void testEditUserPassword() throws Exception {
     this.mockMvc.perform(get("/user/edit/{id}", new Long("1")).with(csrf()))
                 .andExpect(status().isOk());
+  }
+
+  @Test
+  @WithMockUser(username="fake@mail.com", authorities="USER")
+  public void testEditUserPasswordNoUserExists() throws Exception {
+    this.mockMvc.perform(get("/user/edit/{id}", new Long("5")).with(csrf()))
+                .andExpect(status().isNotFound());
+  }
+
+  @Test
+  @WithMockUser(username="fake@mail.com", authorities="USER")
+  public void testEditUserPasswordRestrictedUser() throws Exception {
+    this.mockMvc.perform(get("/user/edit/{id}", new Long("2")).with(csrf()))
+                .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithMockUser(username="fake@mail.com", authorities="USER")
+  public void testEditUserPasswordPost() throws Exception {
+    this.mockMvc.perform(post("/user/edit/{id}", new Long("1"))
+                        .param("accountId","1")
+                        .param("currentPassword", "p@ssw0rd")
+                        .param("newPassword", "p@ssw0rd1")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    verify(this.userRepository, times(1)).updatePassword(any(String.class), any(Long.class));
+  }
+
+  @Test
+  @WithMockUser(username="fake@mail.com", authorities="USER")
+  public void testEditUserPasswordPostBadData() throws Exception {
+    this.mockMvc.perform(post("/user/edit/{id}", new Long("1"))
+                        .param("accountId","1")
+                        .param("currentPassword", "p@ssw0rd321")
+                        .param("newPassword", "p@ssw0rd1")
+                        .with(csrf()))
+                .andExpect(model().attributeHasFieldErrors("passForm", "currentPassword"))
+                .andExpect(status().isOk());
+  }
+
+  @Test
+  @WithMockUser(username="fake@mail.com", authorities="USER")
+  public void testEditUserPasswordNoUserExistsPost() throws Exception {
+    this.mockMvc.perform(get("/user/edit/{id}", new Long("5")).with(csrf()))
+                .andExpect(status().isNotFound());
+  }
+
+  @Test
+  @WithMockUser(username="fake@mail.com", authorities="USER")
+  public void testEditUserPasswordRestrictedUserPost() throws Exception {
+    this.mockMvc.perform(get("/user/edit/{id}", new Long("2")).with(csrf()))
+                .andExpect(status().isForbidden());
   }
 }
