ff071f18c9b55a9ce29bece94aec5479434f5305
rpiper <rpiper@uofs-10-227-176-250.usask.ca>
2017-12-07 19:12:15 -0600

    Fix issue where id was being incorrectly referenced.

--- src/test/java/com/cylinder/crmusers/model/CrmUserRepositoryTests.java
+++ src/test/java/com/cylinder/crmusers/model/CrmUserRepositoryTests.java
@@ -35,31 +35,29 @@ public class CrmUserRepositoryTests extends RespositoryTests {
         CrmUser user = userRepository.findByEmail("testball@gmail.com");
         assertEquals(user == null, false);
         assertEquals(user.getLastName() == "McGee", true);
-        assertEquals(user.getAccountId() == 0, true);
         user = userRepository.findByEmail("not@gmail.com");
         assertEquals(user == null, true);
     }
 
     @Test
     public void testFindByAccountId() {
-        Long accountId = new Long("0");
+        Long accountId = new Long("2");
         CrmUser user = userRepository.findByAccountId(accountId);
         assertEquals(user == null, false);
         assertEquals(user.getLastName() == "McGee", true);
-        assertEquals(user.getAccountId() == 0, true);
-        user = userRepository.findByAccountId(new Long("1"));
+        user = userRepository.findByAccountId(new Long("3"));
         assertEquals(user == null, true);
     }
 
     @Test
     public void testExistsByAccountId() {
-        assertEquals(userRepository.existsByAccountId(new Long("0")), true);
-        assertEquals(userRepository.existsByAccountId(new Long("1")), false);
+        assertEquals(userRepository.existsByAccountId(new Long("1")), true);
+        assertEquals(userRepository.existsByAccountId(new Long("2")), false);
     }
 
     @Test
     public void testExistsByAccountIdAndEmail() {
-        assertEquals(userRepository.existsByAccountIdAndEmail(new Long("0"), "testball@gmail.com"), true);
+        assertEquals(userRepository.existsByAccountIdAndEmail(new Long("4"), "testball@gmail.com"), true);
         assertEquals(userRepository.existsByAccountIdAndEmail(new Long("11"), "testfake@gmail.com"), false);
         assertEquals(userRepository.existsByAccountIdAndEmail(new Long("1"), "testfake@gmail.com"), false);
     }
